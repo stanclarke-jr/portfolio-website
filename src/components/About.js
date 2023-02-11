503a84adfbcf032572d0a4d853be1c548d6e3e7d
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-11 16:47:42 -0500

    fix: Add id to section for react-scroll

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import Image from '../assets/img/about.webp';
 
 const About = () => {
   return (
-    <section className="section bg-secondary">
+    <section id="about" className="section bg-secondary">
       <div className="container mx-auto">
         {/* Image */}
         <div className="flex flex-col xl:flex-row gap-24">
