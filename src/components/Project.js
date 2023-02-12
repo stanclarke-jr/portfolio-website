94440fc5fdc7eace3abdbb3d31727714874d189f
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-12 13:45:12 -0500

    feat: Portfolio/Projects section done

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,13 @@
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({ item }) => {
+  return (
+    <div key={item.id} className="flex flex-col items-center text-center">
+      <div className="mb-6">
+        <img className="rounded-2xl" src={item.image} alt={item.name} />
+      </div>
+      <p className="capitalize text-accent text-sm mb-2">{item.category}</p>
+      <h3 className="text-2xl font-semibold capitalize mb-8">{item.name}</h3>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,9 +8,7 @@ import Project from './Project';
 const Projects = () => {
   const [filter, setFilter] = useState({ name: 'all' });
   const [projects, setProjects] = useState([]);
-  const [active, setActive] = useState(false);
-
-  console.log(filter.name);
+  const [active, setActive] = useState(0);
 
   useEffect(() => {
     // Fetch projects based on filter
@@ -24,16 +22,39 @@ const Projects = () => {
     }
   }, [filter]);
 
+  const handleClick = (e, index) => {
+    setFilter({ name: e.target.textContent.toLowerCase() });
+    setActive(index);
+  };
+
   return (
     <div>
       {/* Projects nav */}
       <nav className="mb-12 max-w-xl mx-auto">
         <ul className="flex flex-col md:flex-row justify-evenly items-center text-white">
-          {projectsNav.map((item, index) => {
-            return <li key={index}>{item.name}</li>;
+          {projectsNav.map((category, index) => {
+            return (
+              <li
+                key={index}
+                onClick={(e) => {
+                  handleClick(e, index);
+                }}
+                className={`${
+                  active === index ? 'active' : ''
+                } cursor-pointer capitalize m-4`}
+              >
+                {category.name}
+              </li>
+            );
           })}
         </ul>
       </nav>
+      {/* Projects grid */}
+      <section className="grid lg:grid-cols-3 gap-y-12 lg:gap-x-8 lg:gap-y-8">
+        {projects.map((item) => {
+          return <Project key={item.id} item={item} />;
+        })}
+      </section>
     </div>
   );
 };
